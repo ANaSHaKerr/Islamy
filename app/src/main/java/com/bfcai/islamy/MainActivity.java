7e64cd6fadf1363fa9598c31fd64c6536a068bea
MahmoudSaberr <saberm19071911@gmail.com>
2021-08-19 07:18:49 +0200

    اسلامي

--- app/src/main/java/com/bfcai/islamy/MainActivity.java
+++ app/src/main/java/com/bfcai/islamy/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     CardView islamicStories,islamicNote;
     NavigationView navigationView ;
 
+    //اسلامي
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
